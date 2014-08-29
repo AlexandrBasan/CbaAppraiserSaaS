7ffe39263eaddc7122f007bd31c2b72c46512d82
Proalab <alexandr.basan@gmail.com>
2014-08-29 23:46:34 +0300

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CbaAppraiserSoft::Application.config.secret_key_base = '6fe89dd98ecb4bed2a41bde59c2c7e496af4105dcfc7f462d5e1cd3346adf26ebbf62a25d7eec2fb7ed8b894ad63d5ac3b4dd65159654afb4615c38d47f0fa15'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CbaAppraiserSoft::Application.config.session_store :cookie_store, key: '_cba_appraiser_soft_session'

c03a16cfdcca9263f208c0befd344c0020fd8fe7
Proalab <alexandr.basan@gmail.com>
2014-08-30 12:38:50 +0300

    change code for ruby 4

--- app/models/test.rb
+++ app/models/test.rb
@@ -10,21 +10,22 @@ class Test < ActiveRecord::Base
   end
 
   def self.import(file)
+    allowed_attributes = [ "id","col_1_s","col_2_i","created_at","updated_at"]
     spreadsheet = open_spreadsheet(file)
     header = spreadsheet.row(1)
     (2..spreadsheet.last_row).each do |i|
       row = Hash[[header, spreadsheet.row(i)].transpose]
       product = find_by_id(row["id"]) || new
-      product.attributes = row.to_hash.slice(*accessible_attributes)
+      product.attributes = row.to_hash.select { |k,v| allowed_attributes.include? k }
       product.save!
     end
   end
 
   def self.open_spreadsheet(file)
     case File.extname(file.original_filename)
-      when ".csv" then Csv.new(file.path, nil, :ignore)
-      when ".xls" then Excel.new(file.path, nil, :ignore)
-      when ".xlsx" then Excelx.new(file.path, nil, :ignore)
+      when ".csv" then Roo::Csv.new(file.path)
+      when ".xls" then Roo::Excel.new(file.path, nil, :ignore)
+      when ".xlsx" then Roo::Excelx.new(file.path, nil, :ignore)
       else raise "Unknown file type: #{file.original_filename}"
     end
   end

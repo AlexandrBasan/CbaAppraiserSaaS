78740b135099d25a71545a8d432b35b64b74019e
Peluhnya Evgen <peluhnya@gmail.com>
2014-09-25 12:32:00 +0300

    update calc

--- app/controllers/calc_pages_controller.rb
+++ app/controllers/calc_pages_controller.rb
@@ -2,5 +2,8 @@ class CalcPagesController < ApplicationController
   def calc_apartment
     @apartments = Apartment.all
     @anaprtments = Anaprtment.all
+    @torg = -3
+    @tiphouses = Tiphouse.all
+    @repairs = Repair.all
   end
 end

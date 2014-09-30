427a809690a32fa702dcda0ca83ff196f988f7a4
Peluhnya Evgen <peluhnya@gmail.com>
2014-09-30 21:10:24 +0300

    update

--- app/controllers/systems_controller.rb
+++ app/controllers/systems_controller.rb
@@ -69,6 +69,7 @@ class SystemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def system_params
-      params.require(:system).permit(:torg_apartment, :torg_earth, :torg_house, :floor_apartment, :location, :infrastructure)
+      params.require(:system).permit(:torg_apartment, :torg_earth, :torg_house, :floor_apartment,
+                                     :location, :infrastructure, :location_house, :infrastructura_house)
     end
 end

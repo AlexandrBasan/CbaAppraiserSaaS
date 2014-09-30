6f90ebf4c4d32f73c35ae9b842bffef878f56fad
Proalab <alexandr.basan@gmail.com>
2014-09-30 14:54:37 +0300

    test decimal to data cnvertion

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -6,12 +6,12 @@ class Apartment < ActiveRecord::Base
   def self.to_csv(options = {})
     (CSV.generate(options) do |csv|
       # column headers for table - language
-
       column_header = [ "Kод oбеспечення ", "Новый код ", "Номер счета ", "Альтернативный номер счета ", "Тип обеспечения ", "Область ", "Район ", "Тип населенного пунтка ", "Город ", "Тип улицы ", "Название улицы ", "Номер дома ", "Номер дома ", "Номер квартиры ", "Площадь ", "Жилая площадь, кв.м ", "Кол. комнат ", "Этаж ", "Этажность ", "Серия дома ", "№ района ", "Рыночная стоимость, грн ", "Рыночная стоимость, дол.США ", "Рыночная стоимость, евро "]
       csv << column_names
       csv << column_header
       all.each do |product|
-        csv << product.attributes.values_at(*column_names)
+        conv_prod = product.attributes.values_at(*column_names).map(&:to_s)
+        csv << conv_prod
       end
     end).encode('Windows-1251')
   end

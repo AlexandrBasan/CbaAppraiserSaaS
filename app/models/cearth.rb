74c5be378f851707be50ac684ac6f026e806ac24
Peluhnya Evgen <peluhnya@gmail.com>
2014-09-30 11:06:09 +0300

    update calc earth

--- app/models/cearth.rb
+++ app/models/cearth.rb
@@ -7,8 +7,8 @@ class Cearth < ActiveRecord::Base
     self.auction = System.first.torg_earth
     self.location = System.first.location
     self.infrastructure = System.first.infrastructure
-    @numb= 0.1
-    self.diff_area = ((self.aneart.area.to_s.to_d/self.earth.total_area.to_s.to_d)-1)*100
+
+    self.diff_area = ((self.aneart.area.to_s.to_d/self.earth.total_area.to_s.to_d)**0.1-1)*100
 end
 
 
